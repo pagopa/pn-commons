07a6edb420c6c650287eb3bc85fafa0d9adac815
marco-vit-pagopa <marco.vit@pagopa.it>
2021-08-24 12:58:42 +0200

    Tolto codice superfluo

--- src/main/java/it/pagopa/pn/commons/configs/aws/AwsConfigs.java
+++ src/main/java/it/pagopa/pn/commons/configs/aws/AwsConfigs.java
@@ -1,10 +1,12 @@
 package it.pagopa.pn.commons.configs.aws;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Data
+@Getter
+@Setter
 @Configuration
 @ConfigurationProperties("aws")
 public class AwsConfigs {
