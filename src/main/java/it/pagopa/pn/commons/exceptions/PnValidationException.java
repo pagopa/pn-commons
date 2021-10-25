ef14b1a5e03206c4d0d9bfffc45863d1bac37a82
simonerondinella <92715398+simonerondinella@users.noreply.github.com>
2021-10-25 10:06:16 +0200

    Feature/pn 330bis (#18)
    * Modifica readme
    
    * Modifica a PnValidationException

--- src/main/java/it/pagopa/pn/commons/exceptions/PnValidationException.java
+++ src/main/java/it/pagopa/pn/commons/exceptions/PnValidationException.java
@@ -6,14 +6,20 @@ import java.util.Set;
 
 public class PnValidationException extends IllegalArgumentException {
 
+    private final transient String validationTargetId;
     private final transient Set<ConstraintViolation> validationErrors;
 
-    public <T> PnValidationException( Set<ConstraintViolation<T>> validationErrors ) {
+    public <T> PnValidationException(String validationTargetId, Set<ConstraintViolation<T>> validationErrors) {
         super( validationErrors.toString() );
+        this.validationTargetId = validationTargetId;
         this.validationErrors = Collections.unmodifiableSet( validationErrors );
     }
 
     public Set<ConstraintViolation> getValidationErrors() {
         return validationErrors;
+    }
+
+    public String getValidationTargetId() {
+        return validationTargetId;
     }
 }
