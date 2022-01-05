87d30959e395e76e2bba8bef4c51fa5231a818b0
Simone Rondinella <simone.rondinella@pagopa.it>
2022-01-05 09:36:32 +0100

    Modifica addressbook

--- src/main/java/it/pagopa/pn/commons/pnclients/addressbook/AddressBook.java
+++ src/main/java/it/pagopa/pn/commons/pnclients/addressbook/AddressBook.java
@@ -1,10 +1,10 @@
 package it.pagopa.pn.commons.pnclients.addressbook;
 
 import it.pagopa.pn.api.dto.addressbook.AddressBookEntry;
+import it.pagopa.pn.api.dto.notification.NotificationSender;
 
 import java.util.Optional;
 
 public interface AddressBook {
-
-    Optional<AddressBookEntry> getAddresses(String taxId );
+    Optional<AddressBookEntry> getAddresses(String taxId, NotificationSender sender);
 }
