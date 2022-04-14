02ae23447ebbdc3b766e0fe66893a8737e78606f
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-04-14 14:31:47 +0200

    PN-567: Utility classes for audit log

---
+++ src/main/java/it/pagopa/pn/commons/log/PnAuditMarkerFactory.java
@@ -0,0 +1,34 @@
+package it.pagopa.pn.commons.log;
+
+import org.slf4j.MarkerFactory;
+
+/**
+ * Class for mark audit logs base on retention period
+ */
+public class PnAuditMarkerFactory {
+    enum Marker {
+        AUDIT10Y, AUDIT5Y;
+
+        private final org.slf4j.Marker mark;
+
+        Marker() {
+            this.mark = MarkerFactory.getMarker(this.name());
+        }
+    }
+
+    /**
+     * Ten years retention audit logs for legal facts data
+     * @return 10 years log marker
+     */
+    public static org.slf4j.Marker get10yMarker() {
+        return Marker.AUDIT10Y.mark;
+    }
+
+    /**
+     * Five years retention audit logs
+     * @return 5 years log marker
+     */
+    public static org.slf4j.Marker get5yMarker() {
+        return  Marker.AUDIT5Y.mark;
+    }
+}
