70c8f516990e9d6cf43767d3d31291da8b8591e0
Simone Rondinella <simone.rondinella@pagopa.it>
2022-05-09 20:24:50 +0200

    Inseriti metodi statici per convert Date Instant

--- src/main/java/it/pagopa/pn/commons/utils/DateUtils.java
+++ src/main/java/it/pagopa/pn/commons/utils/DateUtils.java
@@ -5,8 +5,9 @@ import org.jetbrains.annotations.NotNull;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
-public abstract class DateFormatUtils {
+public abstract class DateUtils {
     public static final String yyyyMMddHHmmssSSSZ = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 
     @NotNull
@@ -14,5 +15,13 @@ public abstract class DateFormatUtils {
         return DateTimeFormatter.ofPattern(pattern)
                 .withZone(ZoneId.systemDefault())
                 .format(dateToFormat);
+    }
+    
+    public static Date convertInstantToDate(Instant dateToConvert){
+        return dateToConvert != null ? Date.from(dateToConvert) : null;
+    }
+
+    public static Instant convertDateToInstant(Date dateToConvert){
+        return dateToConvert != null ? dateToConvert.toInstant() : null;
     }
 }
