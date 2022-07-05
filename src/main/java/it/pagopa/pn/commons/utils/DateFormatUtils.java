741b38946bfab4d145c44847174366aa4f8ed275
Simone Rondinella <simone.rondinella@pagopa.it>
2022-07-05 16:37:27 +0200

    PN-1657 Aggiunta formatter per date

--- src/main/java/it/pagopa/pn/commons/utils/DateFormatUtils.java
+++ src/main/java/it/pagopa/pn/commons/utils/DateFormatUtils.java
@@ -3,11 +3,15 @@ package it.pagopa.pn.commons.utils;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public abstract class DateFormatUtils {
-    public static final String yyyyMMddHHmmssSSSZ = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+    private DateFormatUtils() {}
+    
+    public static final ZoneId italianZoneId =  ZoneId.of("Europe/Rome");
 
     @NotNull
     public static String formatInstantToString(Instant dateToFormat, String pattern) {
@@ -19,5 +23,45 @@ public abstract class DateFormatUtils {
     @NotNull
     public static Instant getInstantFromString(String dateToFormat, String pattern) {
         return Instant.from( DateTimeFormatter.ofPattern(pattern).withZone(ZoneId.of("UTC") ).parse(dateToFormat));
+    }
+
+    public static String formatDate(Instant instant)
+    {
+        if (instant == null)
+            return null;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
+        return LocalDate.ofInstant(instant, italianZoneId).format(formatter);
+    }
+
+    public static String formatTime(ZonedDateTime datetime)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+        return datetime.format(formatter.withZone(italianZoneId));
+    }
+
+    public static ZonedDateTime parseDate(String date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
+        LocalDate locDate = LocalDate.parse(date, formatter);
+
+        return locDate.atStartOfDay(italianZoneId);
+    }
+
+    public static ZonedDateTime atStartOfDay(Instant instant)
+    {
+        LocalDate locDate = LocalDate.ofInstant(instant, italianZoneId);
+        return locDate.atStartOfDay(italianZoneId);
+    }
+
+    public static ZonedDateTime parseTime(String date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+        return formatter.parse(date, ZonedDateTime::from);
+    }
+
+    public static ZonedDateTime parseInstantToZonedDateTime(Instant date)
+    {
+        return date.atZone(italianZoneId);
     }
 }
