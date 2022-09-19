6e20123bafe868334912fbe71667fc8a3278fa37
Simone Rondinella <simone.rondinella@pagopa.it>
2022-09-19 11:10:45 +0200

    PN-1774 Aggiunti format di date

--- src/main/java/it/pagopa/pn/commons/utils/DateFormatUtils.java
+++ src/main/java/it/pagopa/pn/commons/utils/DateFormatUtils.java
@@ -2,16 +2,15 @@ package it.pagopa.pn.commons.utils;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 public abstract class DateFormatUtils {
-    private DateFormatUtils() {}
-    
-    public static final ZoneId italianZoneId =  ZoneId.of("Europe/Rome");
+    private DateFormatUtils() {
+    }
+
+    public static final ZoneId utcZoneId = ZoneId.of("UTC");
+    public static final ZoneId italianZoneId = ZoneId.of("Europe/Rome");
 
     @NotNull
     public static String formatInstantToString(Instant dateToFormat, String pattern) {
@@ -22,11 +21,10 @@ public abstract class DateFormatUtils {
 
     @NotNull
     public static Instant getInstantFromString(String dateToFormat, String pattern) {
-        return Instant.from( DateTimeFormatter.ofPattern(pattern).withZone(ZoneId.of("UTC") ).parse(dateToFormat));
+        return Instant.from(DateTimeFormatter.ofPattern(pattern).withZone(ZoneId.of("UTC")).parse(dateToFormat));
     }
 
-    public static String formatDate(Instant instant)
-    {
+    public static String formatDate(Instant instant) {
         if (instant == null)
             return null;
 
@@ -34,34 +32,45 @@ public abstract class DateFormatUtils {
         return LocalDate.ofInstant(instant, italianZoneId).format(formatter);
     }
 
-    public static String formatTime(ZonedDateTime datetime)
-    {
+    @NotNull
+    public static ZonedDateTime setSpecificTimeToDate(ZonedDateTime dateTime, int hour, int minute, int second, int nanoOfSecond) {
+        ZonedDateTime endDateTime = dateTime;
+        endDateTime = endDateTime.withHour(hour);
+        endDateTime = endDateTime.withMinute(minute);
+        endDateTime = endDateTime.withSecond(second);
+        endDateTime = endDateTime.withNano(nanoOfSecond);
+        return endDateTime;
+    }
+
+    public static String formatTime(ZonedDateTime datetime) {
         DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
         return datetime.format(formatter.withZone(italianZoneId));
     }
 
-    public static ZonedDateTime parseDate(String date)
-    {
+    public static ZonedDateTime parseDate(String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
         LocalDate locDate = LocalDate.parse(date, formatter);
 
         return locDate.atStartOfDay(italianZoneId);
     }
 
-    public static ZonedDateTime atStartOfDay(Instant instant)
-    {
+    public static ZonedDateTime atStartOfDay(Instant instant) {
         LocalDate locDate = LocalDate.ofInstant(instant, italianZoneId);
         return locDate.atStartOfDay(italianZoneId);
     }
 
-    public static ZonedDateTime parseTime(String date)
-    {
+    public static ZonedDateTime parseTime(String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
         return formatter.parse(date, ZonedDateTime::from);
     }
 
-    public static ZonedDateTime parseInstantToZonedDateTime(Instant date)
-    {
+    public static Instant getEndOfTheDay() {
+        ZonedDateTime todayUtc = LocalDateTime.now().atZone(utcZoneId);
+        ZonedDateTime endOfDay = todayUtc.with(LocalTime.MAX);
+        return endOfDay.toInstant();
+    }
+
+    public static ZonedDateTime parseInstantToZonedDateTime(Instant date) {
         return date.atZone(italianZoneId);
     }
 }
