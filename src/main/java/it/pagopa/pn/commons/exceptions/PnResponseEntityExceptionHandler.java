136067827e7888fff5527eeda8cbaaed65b6f581
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-10-05 12:02:15 +0200

    test sonar bug

--- src/main/java/it/pagopa/pn/commons/exceptions/PnResponseEntityExceptionHandler.java
+++ src/main/java/it/pagopa/pn/commons/exceptions/PnResponseEntityExceptionHandler.java
@@ -26,7 +26,7 @@ public class PnResponseEntityExceptionHandler {
 
     public PnResponseEntityExceptionHandler(ExceptionHelper exceptionHelper) {
         this.exceptionHelper = exceptionHelper;
-        if (true) {
+        if (1==1) {
             objectMapper.findAndRegisterModules();
             objectMapper
                     .configOverride(OffsetDateTime.class)
