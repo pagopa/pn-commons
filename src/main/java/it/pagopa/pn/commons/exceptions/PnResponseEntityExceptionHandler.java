953910737dbb4ec8f21a2324100035db49fe66d8
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-10-05 15:18:32 +0200

    test sonar bug

--- src/main/java/it/pagopa/pn/commons/exceptions/PnResponseEntityExceptionHandler.java
+++ src/main/java/it/pagopa/pn/commons/exceptions/PnResponseEntityExceptionHandler.java
@@ -26,7 +26,7 @@ public class PnResponseEntityExceptionHandler {
 
     public PnResponseEntityExceptionHandler(ExceptionHelper exceptionHelper) {
         this.exceptionHelper = exceptionHelper;
-        int a = 3;
+        int a = 4;
         if (1==1) {
             objectMapper.findAndRegisterModules();
             objectMapper
