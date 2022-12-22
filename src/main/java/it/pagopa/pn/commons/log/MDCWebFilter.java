314cf095c7270e9f486873da69dc34c54685fafd
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2022-12-22 10:11:19 +0100

    PN-2895 aggiunti altri headers nei log

--- src/main/java/it/pagopa/pn/commons/log/MDCWebFilter.java
+++ src/main/java/it/pagopa/pn/commons/log/MDCWebFilter.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -28,12 +29,28 @@ public class MDCWebFilter implements WebFilter {
 
     public static final String MDC_TRACE_ID_KEY = "trace_id";
     public static final String MDC_JTI_KEY = "jti";
+    public static final String MDC_PN_UID_KEY = "pn-uid";
+    public static final String MDC_CX_ID_KEY = "cx-id";
+    public static final String MDC_PN_CX_TYPE_KEY = "pn-cx-type";
+    public static final String MDC_PN_CX_GROUPS_KEY = "pn-cx-groups";
 
     @Value("${pn.log.trace-id-header}")
     private String traceIdHeader;
 
     @Value("${pn.log.jti-header}")
     private String jtiHeader;
+
+    @Value("${pn.log.pn-uid-header}")
+    private String pnUidHeader;
+
+    @Value("${pn.log.cx-id-header}")
+    private String cxIdHeader;
+
+    @Value("${pn.log.pn-cx-type-header}")
+    private String pnCxTypeHeader;
+
+    @Value("${pn.log.pn-cx-groups-header}")
+    private String pnCxGroupsHeader;
 
     private static final String MDC_CONTEXT_REACTOR_KEY = MDCWebFilter.class.getName();
 
@@ -53,34 +70,75 @@ public class MDCWebFilter implements WebFilter {
     public @NotNull Mono<Void> filter(@NotNull ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
         final String notFoundGeneratedTraceId = "trace_id:" + UUID.randomUUID();
         Runnable mdcSetter = () -> {
-            List<String> traceIdHeaders = serverWebExchange.getRequest().getHeaders().get(traceIdHeader);
+            HttpHeaders requestHeaders = serverWebExchange.getRequest().getHeaders();
+            List<String> traceIdHeaders = requestHeaders.get(traceIdHeader);
+
             if (traceIdHeaders != null) {
                 MDC.put(MDC_TRACE_ID_KEY, traceIdHeaders.get(0));
             }
             else
             {
+
                 log.debug("trace id header not found, generating internal trace_id={}", notFoundGeneratedTraceId);
                 MDC.put(MDC_TRACE_ID_KEY, notFoundGeneratedTraceId);
             }
-            List<String> jtiIdHeaders = serverWebExchange.getRequest().getHeaders().get(jtiHeader);
+
+            List<String> jtiIdHeaders = requestHeaders.get(jtiHeader);
             if (jtiIdHeaders != null) {
                 MDC.put(MDC_JTI_KEY, jtiIdHeaders.get(0));
+            }
+            List<String> uidHeaders = requestHeaders.get(pnUidHeader);
+            if(uidHeaders != null) {
+                MDC.put(MDC_PN_UID_KEY, uidHeaders.get(0));
+            }
+            List<String> cxIdHeaders = requestHeaders.get(cxIdHeader);
+            if(cxIdHeaders != null) {
+                MDC.put(MDC_CX_ID_KEY, cxIdHeaders.get(0));
+            }
+            List<String> pnCxTypeHeaders = requestHeaders.get(pnCxTypeHeader);
+            if(pnCxTypeHeaders != null) {
+                MDC.put(MDC_PN_CX_TYPE_KEY, pnCxTypeHeaders.get(0));
+            }
+            List<String> pnCxGroupsHeaders = requestHeaders.get(pnCxGroupsHeader);
+            if(pnCxGroupsHeaders != null) {
+                MDC.put(MDC_PN_CX_GROUPS_KEY, pnCxGroupsHeaders.get(0));
             }
         };
 
         Consumer<SignalType> mdcCleaner = ignored -> {
             MDC.remove(MDC_TRACE_ID_KEY);
             MDC.remove(MDC_JTI_KEY);
+            MDC.remove(MDC_PN_UID_KEY);
+            MDC.remove(MDC_CX_ID_KEY);
+            MDC.remove(MDC_PN_CX_TYPE_KEY);
+            MDC.remove(MDC_PN_CX_GROUPS_KEY);
         };
 
         return webFilterChain.filter(serverWebExchange)
                 .doFirst(mdcSetter)
                 .contextWrite(ctx -> {
-                    List<String> jtiIdHeaders = serverWebExchange.getRequest().getHeaders().get(jtiHeader);
+                    HttpHeaders requestHeaders = serverWebExchange.getRequest().getHeaders();
+                    List<String> jtiIdHeaders = requestHeaders.get(jtiHeader);
                     if (jtiIdHeaders != null) {
                         ctx = ctx.put(MDC_JTI_KEY, jtiIdHeaders.get(0));
                     }
-                    List<String> traceIdHeaders = serverWebExchange.getRequest().getHeaders().get(traceIdHeader);
+                    List<String> uidHeaders = requestHeaders.get(pnUidHeader);
+                    if(uidHeaders != null) {
+                        ctx = ctx.put(MDC_PN_UID_KEY, uidHeaders.get(0));
+                    }
+                    List<String> cxIdHeaders = requestHeaders.get(cxIdHeader);
+                    if(cxIdHeaders != null) {
+                        ctx = ctx.put(MDC_CX_ID_KEY, cxIdHeaders.get(0));
+                    }
+                    List<String> pnCxTypeHeaders = requestHeaders.get(pnCxTypeHeader);
+                    if(pnCxTypeHeaders != null) {
+                        ctx = ctx.put(MDC_PN_CX_TYPE_KEY, pnCxTypeHeaders.get(0));
+                    }
+                    List<String> pnCxGroupsHeaders = requestHeaders.get(pnCxGroupsHeader);
+                    if(pnCxGroupsHeaders != null) {
+                        ctx = ctx.put(MDC_PN_CX_GROUPS_KEY, pnCxGroupsHeaders.get(0));
+                    }
+                    List<String> traceIdHeaders = requestHeaders.get(traceIdHeader);
                     if (traceIdHeaders != null) {
                         return ctx.put(MDC_TRACE_ID_KEY, traceIdHeaders.get(0));
                     }
--- src/main/java/it/pagopa/pn/commons/log/MdcContextLifter.java
+++ src/main/java/it/pagopa/pn/commons/log/MdcContextLifter.java
@@ -49,12 +49,22 @@ class MdcContextLifter<T> implements CoreSubscriber<T> {
     private void injectMdc(Runnable task) {
         final Object mdcTraceidVal = coreSubscriber.currentContext().getOrDefault(MDCWebFilter.MDC_TRACE_ID_KEY, null);
         final Object mdcJtiVal = coreSubscriber.currentContext().getOrDefault(MDCWebFilter.MDC_JTI_KEY, null);
+        final Object mdcPnUidVal = coreSubscriber.currentContext().getOrDefault(MDCWebFilter.MDC_PN_UID_KEY, null);
+        final Object mdcCxIdVal = coreSubscriber.currentContext().getOrDefault(MDCWebFilter.MDC_CX_ID_KEY, null);
+        final Object mdcPnCxTypeVal = coreSubscriber.currentContext().getOrDefault(MDCWebFilter.MDC_PN_CX_TYPE_KEY, null);
+        final Object mdcPnCxGroupsVal = coreSubscriber.currentContext().getOrDefault(MDCWebFilter.MDC_PN_CX_GROUPS_KEY, null);
         // NB: MDC supporta il value null, a patto che chi lo usa lo supporti a sua volta.
         // si sceglie comunque di inserire i valori solo se presenti gestendo i vari casi
-        if(mdcTraceidVal != null && mdcJtiVal != null ) {
+        if(mdcTraceidVal != null && mdcJtiVal != null && mdcPnUidVal != null && mdcCxIdVal != null && mdcPnCxTypeVal != null
+        && mdcPnCxGroupsVal != null) {
 
             try(MDC.MDCCloseable ignored = MDC.putCloseable(MDCWebFilter.MDC_TRACE_ID_KEY, mdcTraceidVal.toString());
-                MDC.MDCCloseable ignored1 = MDC.putCloseable(MDCWebFilter.MDC_JTI_KEY, mdcJtiVal.toString())) {
+                MDC.MDCCloseable ignored1 = MDC.putCloseable(MDCWebFilter.MDC_JTI_KEY, mdcJtiVal.toString());
+                MDC.MDCCloseable ignored2 = MDC.putCloseable(MDCWebFilter.MDC_PN_UID_KEY, mdcPnUidVal.toString());
+                MDC.MDCCloseable ignored3 = MDC.putCloseable(MDCWebFilter.MDC_CX_ID_KEY, mdcCxIdVal.toString());
+                MDC.MDCCloseable ignored4 = MDC.putCloseable(MDCWebFilter.MDC_PN_CX_TYPE_KEY, mdcPnCxTypeVal.toString());
+                MDC.MDCCloseable ignored5 = MDC.putCloseable(MDCWebFilter.MDC_PN_CX_GROUPS_KEY, mdcPnCxGroupsVal.toString())
+            ) {
                 task.run();
             }
         } else if (mdcTraceidVal != null) {
@@ -65,7 +75,24 @@ class MdcContextLifter<T> implements CoreSubscriber<T> {
             try(MDC.MDCCloseable ignored = MDC.putCloseable(MDCWebFilter.MDC_JTI_KEY, mdcJtiVal.toString())) {
                 task.run();
             }
-        } else {
+        } else if (mdcPnUidVal != null) {
+            try(MDC.MDCCloseable ignored = MDC.putCloseable(MDCWebFilter.MDC_PN_UID_KEY, mdcPnUidVal.toString())) {
+                task.run();
+            }
+        } else if (mdcCxIdVal != null) {
+            try(MDC.MDCCloseable ignored = MDC.putCloseable(MDCWebFilter.MDC_CX_ID_KEY, mdcCxIdVal.toString())) {
+                task.run();
+            }
+        } else if (mdcPnCxTypeVal != null) {
+            try(MDC.MDCCloseable ignored = MDC.putCloseable(MDCWebFilter.MDC_PN_CX_TYPE_KEY, mdcPnCxTypeVal.toString())) {
+                task.run();
+            }
+        } else if (mdcPnCxGroupsVal != null) {
+            try(MDC.MDCCloseable ignored = MDC.putCloseable(MDCWebFilter.MDC_PN_CX_GROUPS_KEY, mdcPnCxGroupsVal.toString())) {
+                task.run();
+            }
+        }
+        else {
             task.run();
         }
     }
