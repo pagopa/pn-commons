50e8d13df994ff51611fe110b410427bc0dd971f
BaldiVi <vittorio.baldi@emea.nttdata.com>
2023-02-02 10:29:26 +0100

    Test codici fiscali omocodici

--- src/test/java/it/pagopa/pn/commons/utils/ValidateUtilsTest.java
+++ src/test/java/it/pagopa/pn/commons/utils/ValidateUtilsTest.java
@@ -9,23 +9,32 @@ class ValidateUtilsTest {
     /**
      * Method under test: {@link ValidateUtils#validate(String)}
      */
-    @Test
-    void testValidateCf() {
-        assertTrue(ValidateUtils.validate("FRNSST75D50A717N"));
 
-        Assertions.assertFalse(ValidateUtils.validate(""));
+    @Test
+    void testValidateCfOmocodici(){
+        assertTrue(ValidateUtils.validate("MRNLCC00A01H50MJ"));
+        assertTrue(ValidateUtils.validate("MRNLCU00A01H50MB"));
+        assertTrue(ValidateUtils.validate("BRNLCU00A01H50MJ"));
+        assertTrue(ValidateUtils.validate("MRNLCU00A01H90MJ"));
+    }
+
+    @Test
+    void testValidateCfFalse() {
         Assertions.assertFalse(ValidateUtils.validate("@@@"));
         Assertions.assertFalse(ValidateUtils.validate("@@@@@@@@@@@@@@@@"));
         Assertions.assertFalse(ValidateUtils.validate("@@@@@@@@@@@"));
         Assertions.assertFalse(ValidateUtils.validate("MRORSS00A00A000V"));
         Assertions.assertFalse(ValidateUtils.validate("MRORSS00A+0A000V"));
         Assertions.assertFalse(ValidateUtils.validate("00000+00000"));
-
+        assertTrue(ValidateUtils.validate("FRNSST75D50A717N"));
         assertTrue(ValidateUtils.validate("MRO rSs 00a00 A000U"));
         assertTrue(ValidateUtils.validate("KJWMFE88C50E205S"));
         assertTrue(ValidateUtils.validate("GNNTIS14L02X498V"));
         assertTrue(ValidateUtils.validate("JKNXZK26E16Y097M"));
 
+    }
+    @Test
+    void validateIva(){
         assertTrue(ValidateUtils.validate("00000000000"));
         assertTrue(ValidateUtils.validate("44444444440"));
         assertTrue(ValidateUtils.validate("12345678903"));
@@ -34,7 +43,6 @@ class ValidateUtilsTest {
         assertTrue(ValidateUtils.validate("19258897628"));
         assertTrue(ValidateUtils.validate("08882740981"));
         assertTrue(ValidateUtils.validate("4730 9842  806"));
-
     }
 
 }
