72f42b453ac2b458c308b8f58215d4661eb01c54
fabriziolana <fabrizio.lana@necsy.it>
2023-05-17 16:05:20 +0200

    PN-5858: MDC comuni

--- src/main/java/it/pagopa/pn/commons/utils/MDCUtils.java
+++ src/main/java/it/pagopa/pn/commons/utils/MDCUtils.java
@@ -19,4 +19,41 @@ public class MDCUtils {
         }
         return t;
     }
+
+    public static void setIUN(String iun){
+        MDC.put("ctx_iun", iun);
+    }
+
+    public static void setInternalId(String internalId){
+        MDC.put("ctx_internal_id", internalId);
+    }
+
+    public static void setUid(String uid){
+        MDC.put("ctx_uid", uid);
+    }
+
+    public static void setMandateid(String mandateid){
+        MDC.put("ctx_mandate_id", mandateid);
+    }
+
+    public static void setRecipientIndex(int recipientIndex){
+        MDC.put("ctx_recipient_index", recipientIndex+"");
+    }
+
+    public static void setRequestId(String requestId){
+        MDC.put("ctx_request_id", requestId);
+    }
+
+    public static void setTopic(String topic){
+        MDC.put("ctx_topic", topic);
+    }
+
+    /**
+     * aggiunge all'MDC una coppia chiave valore
+     * @param key chiave da aggiungere, alla quale verrà aggiunto automaticamente il prefisso ctx_
+     * @param value valore da associare alla chiave
+     */
+    public static void setCustom(String key, String value){
+        MDC.put("ctx_" + key, value);
+    }
 }
