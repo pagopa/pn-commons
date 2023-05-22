ef97866a6db39233b03e22334d50a697786f9f0f
Vincenzo.Veniero <vincenzo.veniero@MBPdiVioVeniero.homenet.telecomitalia.it>
2023-05-22 11:59:39 +0200

    Client Server AspectLoggin implementation

--- src/main/java/it/pagopa/pn/commons/utils/ClientAspectLogging.java
+++ src/main/java/it/pagopa/pn/commons/utils/ClientAspectLogging.java
@@ -7,22 +7,63 @@ import org.aspectj.lang.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 
 @Aspect
 @CustomLog
 public class ClientAspectLogging {
-    @Pointcut("execution(* it.pagopa.pn.*.middleware.msclient..*.*(..)) || execution(* it.pagopa.pn.*.*.middleware.msclient..*.*(..))")
+
+    private static final String sensitiveData = "Sensitive Data";
+
+    //GESTIRE POINTCUT CON NUOVI STANDARD
+    @Pointcut("execution(* it.pagopa.pn..generated.openapi.msclient..api.*.*(..))")
     public void client() {
         // all client methods
     }
 
     @Around(value = "client()")
     public Object logAroundClient(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.debug("Client method {} with args: {}", joinPoint.getSignature().toShortString(), joinPoint.getArgs());
-        String endingMessage = "Return client method: {}() = {} ";
+        Object[] arguments = Arrays.stream(joinPoint.getArgs()).toArray();
+        ArrayList<Object> argsDefined = getEvaluableArguments(arguments);
+        log.debug("Client method {} with args: {}", joinPoint.getSignature().toShortString(), argsDefined);
+        String endingMessage = "Return client method: {}() Result: {} ";
         var result = joinPoint.proceed();
+        return this.proceed(joinPoint, result, endingMessage);
+    }
+
+    private void logResult(JoinPoint joinPoint, Object result, String message) {
+        //Case: Mono
+        if (result instanceof Mono<?> monoResult) {
+            monoResult.doOnNext(o -> log.info(message,
+                    joinPoint.getSignature().toShortString(),
+                    o instanceof String ? sensitiveData : o)).subscribe();
+            //Case: Flux
+        } else if (result instanceof Flux<?> fluxResult) {
+            fluxResult.doOnNext(o -> log.info(message,
+                    joinPoint.getSignature().toShortString(),
+                    o instanceof String ? sensitiveData : o)).subscribe();
+            //Case: Other
+        } else {
+            log.info(message, joinPoint.getSignature().toShortString(), result instanceof String ? sensitiveData : result);
+        }
+    }
+
+
+    private ArrayList<Object> getEvaluableArguments(Object[] parameterValue){
+        ArrayList<Object> result = new ArrayList<>();
+        for(Object param: parameterValue){
+            if (!(param instanceof String || param instanceof List<?>)) {
+                result.add(param);
+            }
+        }
+        return result;
+    }
+
+    private Object proceed(ProceedingJoinPoint joinPoint, Object result, String endingMessage) throws Throwable {
         if (result instanceof Mono<?> monoResult) {
             return monoResult.doOnSuccess(o -> {
                         var response = "";
@@ -32,7 +73,7 @@ public class ClientAspectLogging {
                         logResult(joinPoint, response, endingMessage);
                     })
                     .doOnError(o->{
-                        log.warn("Warning: {} ON MONO", o.getMessage());
+                        log.warn("ERRORE ON MONO");
                     });
         }
         else if (result instanceof Flux<?> fluxResult) {
@@ -49,37 +90,6 @@ public class ClientAspectLogging {
         else {
             logResult(joinPoint, result, endingMessage);
             return result;
-        }
-    }
-
-    private void logResult(JoinPoint joinPoint, Object result, String message) {
-        //Case: Mono
-        if (result instanceof Mono<?> monoResult) {
-            monoResult.doOnNext(o -> log.info(message,
-                    joinPoint.getSignature().getDeclaringTypeName(),
-                    joinPoint.getSignature().getName(),
-                    o,
-                    result)).subscribe();
-            //Case: Flux
-        } else if (result instanceof Flux<?> fluxResult) {
-            fluxResult.doOnNext(o -> log.info(message,
-                    joinPoint.getSignature().getDeclaringTypeName(),
-                    joinPoint.getSignature().getName(),
-                    o,
-                    result)).subscribe();
-            //Case: Other
-        } else {
-            log.info(message, joinPoint.getSignature().toShortString(), result);
-        }
-    }
-
-
-    Object maskIfsensitive(Object param){
-        if(param instanceof String){
-            return LogUtils.maskTaxId((String) param);
-        }
-        else {
-            return "****";
         }
     }
 }
---
+++ src/main/java/it/pagopa/pn/commons/utils/ServerAspectLogging.java
@@ -0,0 +1,143 @@
+package it.pagopa.pn.commons.utils;
+
+import lombok.CustomLog;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.adapter.DefaultServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+
+@Aspect
+@CustomLog
+public class ServerAspectLogging {
+
+    private static final String sensitiveData = "Sensitive Data";
+
+    //GESTIRE POINTCUT CON NUOVI STANDARD
+    @Pointcut("execution(* it.pagopa.pn..generated.openapi.server..*.*(..))")
+    public void server() {
+        // all rest controller method
+    }
+
+    @Around(value = "server()")
+    public Object logAroundServer(ProceedingJoinPoint joinPoint) throws Throwable {
+        String process = joinPoint.getSignature().getName();
+        String endingMessage = "Successful API operation: {}(). Result {} ";
+        log.logStartingProcess(process);
+        Method m = ((MethodSignature) joinPoint.getSignature()).getMethod();
+        Object[] arguments = Arrays.stream(joinPoint.getArgs()).toArray();
+        String url = ((DefaultServerWebExchange) arguments[arguments.length-1]).getRequest().getPath().toString();
+
+        //RETRIEVE ARGUMENTS TO PRINT
+        List<Annotation[]> annotationsParams = Arrays.stream(m.getParameterAnnotations()).toList();
+        ArrayList<Object> argsDefined = getEvaluableArguments(annotationsParams, arguments);
+        log.debug("Invoked operationId {} with uri {} with args: {}", joinPoint.getSignature().getName(), url, argsDefined);
+
+        for (Object v : arguments) {
+            if (v instanceof Mono<?> mono) {
+                Mono<?> tempMono = mono.map(value ->{
+                    log.debug("MonoLift value: {} ", value);
+                    return value;
+                });
+                for (int i = 0; i < arguments.length; i++) {
+                    if (arguments[i] instanceof Mono<?>) {
+                        arguments[i] = tempMono;
+                    }
+                }
+                //Proceed with new Mono in arguments
+                var result = joinPoint.proceed(arguments);
+                return this.proceed(joinPoint, result, endingMessage, process);
+            }
+        }
+        //Proceed same arguments
+        var result = joinPoint.proceed();
+        return this.proceed(joinPoint, result, endingMessage, process);
+
+    }
+
+    private void logResult(JoinPoint joinPoint, Object result, String message) {
+        //Case: Mono
+        if (result instanceof Mono<?> monoResult) {
+            monoResult.doOnNext(o -> log.info(message,
+                    joinPoint.getSignature().toShortString(),
+                    o instanceof String ? sensitiveData : result)).subscribe();
+            //Case: Flux
+        } else if (result instanceof Flux<?> fluxResult) {
+            fluxResult.doOnNext(o -> log.info(message,
+                    joinPoint.getSignature().toShortString(),
+                    o instanceof String ? sensitiveData : result)).subscribe();
+            //Case: Other
+        } else {
+            log.info(message, joinPoint.getSignature().toShortString(), result instanceof String ? sensitiveData : result);
+        }
+    }
+
+    private ArrayList<Object> getEvaluableArguments(List<Annotation[]> parametersAnnotation, Object[] parameterValue){
+        ArrayList<Object> result = new ArrayList<>();
+        for(int i = 0; i < parametersAnnotation.size(); i++){
+            final int index = i;
+            List<Annotation> ann = Arrays.stream(parametersAnnotation.get(i)).toList();
+            if (!ann.isEmpty()) {
+                for (Annotation value : ann) {
+                    if (!(value instanceof RequestHeader || value instanceof RequestParam)) {
+                        result.add(parameterValue[index]);
+                        break;
+                    }
+                }
+            }
+            else
+            if(!(parameterValue[index] instanceof ServerWebExchange))
+                result.add(parameterValue[index]);
+        }
+        return result;
+    }
+
+    private Object proceed(ProceedingJoinPoint joinPoint, Object result, String endingMessage, String process) throws Throwable {
+        if (result instanceof Mono<?> monoResult) {
+            return monoResult.doOnSuccess(o -> {
+                        var response = "";
+                        if (Objects.nonNull(o)) {
+                            response = o.toString();
+                        }
+                        logResult(joinPoint, response, endingMessage);
+                        log.logEndingProcess(process);
+                    })
+                    .doOnError(o->{
+                        log.warn("ERRORE ON MONO");
+                    });
+        }
+        else if (result instanceof Flux<?> fluxResult) {
+            return fluxResult.doOnNext(o -> {
+                var response = "";
+                if (Objects.nonNull(o)) {
+                    response = o.toString();
+                }
+                logResult(joinPoint, response, endingMessage);
+                log.logEndingProcess(process);
+            }).doOnError(o->{
+                log.warn("Warning: {} ON FLUX", o.getMessage());
+            });
+        }
+        else {
+            logResult(joinPoint, result, endingMessage);
+            log.logEndingProcess(process);
+            return result;
+        }
+    }
+}
+
