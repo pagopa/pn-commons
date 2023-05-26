ff8978e654eed1d3ab750e4ad442922dd9c37f09
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2023-05-26 18:34:40 +0200

    PN-5514: maven artifact repository relocation

--- scripts/copy-GH2AWS/copyLollipopLibrary.sh
+++ scripts/copy-GH2AWS/copyLollipopLibrary.sh
@@ -7,17 +7,24 @@ ARTIFACTS=(
   it.pagopa.tech.lollipop-consumer-java-sdk:http-verifier:1.0.0-RC1
   it.pagopa.tech.lollipop-consumer-java-sdk:assertion-rest-client-native:1.0.0-RC1
   it.pagopa.tech.lollipop-consumer-java-sdk:identity-service-rest-client-native:1.0.0-RC1
+  it.pagopa.tech:http-signatures:1.1.4
+)
+
+REPOS=(
+  ["lollipop-consumer-java-sdk"]="https://maven.pkg.github.com/pagopa/eng-http-signatures"
+  ["tech"]="https://maven.pkg.github.com/pagopa/eng-http-signatures"
 )
 # https://maven.pkg.github.com/pagopa/eng-http-signatures
 # https://maven.pkg.github.com/pagopa/eng-lollipop-consumer-java-sdk
 
 for ARTIFACT in ${ARTIFACTS[@]}; do
-  mvn -P github org.apache.maven.plugins:maven-dependency-plugin:2.8:get \
-       -DrepoUrl=https://maven.pkg.github.com/pagopa/eng-lollipop-consumer-java-sdk \
-       -Dartifact=$ARTIFACT
   GRP=$(echo $ARTIFACT| cut -d ':' -f 1)
   ART=$(echo $ARTIFACT| cut -d ':' -f 2)
   VER=$(echo $ARTIFACT| cut -d ':' -f 3)
+  REPO=${REPOS[${GRP##*.}]}
+  mvn -P github org.apache.maven.plugins:maven-dependency-plugin:2.8:get \
+       -DrepoUrl=$REPO \
+       -Dartifact=$ARTIFACT
 
   cp "$HOME/.m2/repository/${GRP//.//}/${ART}/${VER}/${ART}-${VER}.jar" /tmp
   cp $HOME/.m2/repository/${GRP//.//}/${ART}/${VER}/${ART}-${VER}.pom /tmp
