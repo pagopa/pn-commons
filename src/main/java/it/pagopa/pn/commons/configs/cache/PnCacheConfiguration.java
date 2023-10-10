5c36dc64562418b0aa729fa07489e07eeb4bf793
Ivan Rosina <ivan.rosina@cinetix-team.it>
2023-10-10 16:31:52 +0200

    moved all cache configuration in PnCacheConfiguration

--- src/main/java/it/pagopa/pn/commons/configs/cache/PnCacheConfiguration.java
+++ src/main/java/it/pagopa/pn/commons/configs/cache/PnCacheConfiguration.java
@@ -6,29 +6,26 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Slf4j
-@AllArgsConstructor
 @Configuration
 @ConditionalOnProperty( name = "pn.cache.enabled", havingValue = "true")
 public class PnCacheConfiguration {
 
-
-	
-	private final CacheConfigs cacheProperties;
-
+	@Value("${pn.cache.cacheNames:}")
+	private List<String> cacheNames;
 
 	@java.lang.SuppressWarnings("java:S5852")
 	@Bean(name = "pnCacheManager")
 	public PnCacheManager cacheManager() {
+		log.info("cacheNames: [{}]", cacheNames);
 		String regEx = "([a-zA-Z]+)(\\()(\\d+)";
 		Pattern pattern = Pattern.compile(regEx);
 		PnCacheManager cacheManager = new PnCacheManager() ;
-		List<String> cacheNames = this.cacheProperties.getCacheNames();
 		Map<String, Integer> config = new HashMap<>();
 		cacheNames.forEach(name -> {
 			Matcher matcher = pattern.matcher(name);
