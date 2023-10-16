d1e73ac4b0b0240ebf8ee8935631e7429692ff83
Vincenzo.Veniero <vincenzo.veniero@Host-007.homenet.telecomitalia.it>
2023-10-16 22:20:25 +0200

    PN-7177: logback configuration wire log

---
+++ src/main/java/it/pagopa/pn/commons/configs/listeners/ApplicationConfigurationListener.java
@@ -0,0 +1,20 @@
+package it.pagopa.pn.commons.configs.listeners;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Objects;
+
+public class ApplicationConfigurationListener implements ApplicationListener<ApplicationStartingEvent> {
+
+    @Override
+    public void onApplicationEvent(@NotNull ApplicationStartingEvent event) {
+        String taskId = System.getenv("ECS_AGENT_URI");
+        if(taskId != null) {
+            System.setProperty("TASK_ID", Objects.requireNonNullElse(taskId.split("/")[4].split("-")[0], "local"));
+        }
+    }
+
+}
