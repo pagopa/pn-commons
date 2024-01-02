40ec3b49c2c39ffb516b024fb3c7c673aca43dd9
Christian La Riccia <christian.lariccia@pagopa.it>
2024-01-02 20:46:38 +0100

    refactor(pn-9338): remove unused imports

--- src/test/java/it/pagopa/pn/commons/utils/LogUtilsTest.java
+++ src/test/java/it/pagopa/pn/commons/utils/LogUtilsTest.java
@@ -1,20 +1,10 @@
 package it.pagopa.pn.commons.utils;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class LogUtilsTest {
