681d0cdfa2dbca9e2fc71837b4ac0b586a18c572
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-12-20 22:47:48 +0100

    Added PnAuditLogEventType for national registries call

--- src/main/java/it/pagopa/pn/commons/log/PnAuditLogEventType.java
+++ src/main/java/it/pagopa/pn/commons/log/PnAuditLogEventType.java
@@ -98,7 +98,11 @@ public enum PnAuditLogEventType {
     AUD_RADD_ACTTRAN(PnAuditLogMarker.AUDIT10Y),
     AUD_RADD_AORTRAN(PnAuditLogMarker.AUDIT10Y),
     AUD_NT_RADD_OPEN(PnAuditLogMarker.AUDIT10Y),
-    AUD_DV_DEC(PnAuditLogMarker.AUDIT5Y);
+    AUD_DV_DEC(PnAuditLogMarker.AUDIT5Y),
+    AUD_NR_PG_PHYSICAL(PnAuditLogMarker.AUDIT10Y),
+    AUD_NR_PG_DIGITAL(PnAuditLogMarker.AUDIT10Y),
+    AUD_NR_PF_PHYSICAL(PnAuditLogMarker.AUDIT10Y),
+    AUD_NR_PF_DIGITAL(PnAuditLogMarker.AUDIT10Y);
 
     
     final org.slf4j.Marker marker;
