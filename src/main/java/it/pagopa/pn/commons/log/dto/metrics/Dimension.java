8940f06c0a66a9431616872ce291f92f18c68480
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2025-04-10 09:40:57 +0200

    [PN-14563] Feat: Added classes for GeneralMetric

---
+++ src/main/java/it/pagopa/pn/commons/log/dto/metrics/Dimension.java
@@ -0,0 +1,18 @@
+package it.pagopa.pn.commons.log.dto.metrics;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dimension {
+    private String name;
+    private String value;
+
+    @Override
+    public String toString() {
+        return String.format("{\"name\":\"%s\",\"value\":\"%s\"}", name, value);
+    }
+}
