d7a52bb3600e65ab0d396fa9a1d9c0f321f5615c
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2025-04-10 15:55:48 +0200

    [PN-14564] Feat: Added metric object for EMF format

---
+++ src/main/java/it/pagopa/pn/commons/log/dto/metrics/Dimension.java
@@ -0,0 +1,17 @@
+package it.pagopa.pn.commons.log.dto.metrics;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dimension {
+    private String name;
+    private String value;
+
+    public String toJsonEmf() {
+        return String.format("\"%s\"", name);
+    }
+}
