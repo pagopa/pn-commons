dcdf7d3fe8a892aae10bfd97781989b38ae99647
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2025-04-11 16:52:45 +0200

    [PN-14567] Feat: Added metrics array to audit log after methods

--- src/main/java/it/pagopa/pn/commons/log/PnAuditLogBuilder.java
+++ src/main/java/it/pagopa/pn/commons/log/PnAuditLogBuilder.java
@@ -1,9 +1,12 @@
 package it.pagopa.pn.commons.log;
 
+import it.pagopa.pn.commons.log.dto.metrics.GeneralMetric;
 import it.pagopa.pn.commons.utils.MDCUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -12,6 +15,7 @@ public class PnAuditLogBuilder {
     private String msg;
     private Object[] arguments;
     private Map<String, String> mdcMap;
+    private List<GeneralMetric> metricsArray;
 
     public PnAuditLogBuilder before(PnAuditLogEventType type, String msg, Object ... arguments) {
         this.type = type;
@@ -21,8 +25,21 @@ public class PnAuditLogBuilder {
         return this;
     }
 
+    public PnAuditLogBuilder before(PnAuditLogEventType type, String msg, List<GeneralMetric> metricsArray, Object ... arguments) {
+        this.type = type;
+        this.msg = msg;
+        this.metricsArray = metricsArray;
+        this.arguments = arguments;
+        this.mdcMap = new HashMap<>();
+        return this;
+    }
+
     public PnAuditLogEvent build() {
-        return new PnAuditLogEvent(type, mdcMap, msg, arguments );
+        if(CollectionUtils.isEmpty(metricsArray)) {
+            return new PnAuditLogEvent(type, mdcMap, msg, arguments );
+        } else {
+            return new PnAuditLogEvent(type, mdcMap, msg, metricsArray, arguments );
+        }
     }
 
     public PnAuditLogBuilder iun(String iun) {
--- src/main/java/it/pagopa/pn/commons/log/PnAuditLogEvent.java
+++ src/main/java/it/pagopa/pn/commons/log/PnAuditLogEvent.java
@@ -1,5 +1,8 @@
 package it.pagopa.pn.commons.log;
 
+import it.pagopa.pn.commons.log.dto.metrics.GeneralMetric;
+
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -11,6 +14,7 @@ public class PnAuditLogEvent {
     private PnAuditLogType level;
     private final String uuid;
     private final Map<String, String> mdc;
+    private List<GeneralMetric> metricsArray;
 
     Object[] getArguments() {
         return arguments;
@@ -30,11 +34,21 @@ public class PnAuditLogEvent {
     PnAuditLogType getLevel() {
         return level;
     }
+    List<GeneralMetric> getMetricsArray() { return metricsArray; }
 
     public PnAuditLogEvent(PnAuditLogEventType type, Map<String, String> mdc, String message, Object... arguments) {
         this.type = type;
         this.mdc = mdc;
         this.message = message;
+        this.arguments = arguments;
+        this.uuid = UUID.randomUUID().toString();
+    }
+
+    public PnAuditLogEvent(PnAuditLogEventType type, Map<String, String> mdc, String message, List<GeneralMetric> metricsArray, Object... arguments) {
+        this.type = type;
+        this.mdc = mdc;
+        this.message = message;
+        this.metricsArray = metricsArray;
         this.arguments = arguments;
         this.uuid = UUID.randomUUID().toString();
     }
@@ -47,16 +61,35 @@ public class PnAuditLogEvent {
         return generateResult(PnAuditLogType.SUCCESS, message, arguments);
     }
 
+    public PnAuditLogEvent generateSuccess(String message, List<GeneralMetric> metricsArray, Object... arguments) {
+        return generateResult(PnAuditLogType.SUCCESS, message, metricsArray, arguments);
+    }
+
     public PnAuditLogEvent generateFailure(String message, Object... arguments) {
         return generateResult(PnAuditLogType.FAILURE, message, arguments);
+    }
+
+    public PnAuditLogEvent generateFailure(String message, List<GeneralMetric> metricsArray, Object... arguments) {
+        return generateResult(PnAuditLogType.FAILURE, message, metricsArray, arguments);
     }
 
     public PnAuditLogEvent generateWarning(String message, Object... arguments) {
         return generateResult(PnAuditLogType.WARNING, message, arguments);
     }
+
+    public PnAuditLogEvent generateWarning(String message, List<GeneralMetric> metricsArray, Object... arguments) {
+        return generateResult(PnAuditLogType.WARNING, message, metricsArray, arguments);
+    }
     
     public PnAuditLogEvent generateResult(PnAuditLogType level, String message, Object... arguments) {
         PnAuditLogEvent resultEvent = new PnAuditLogEvent(type, mdc, message, arguments);
+        resultEvent.originEvent = this;
+        resultEvent.level = level;
+        return resultEvent;
+    }
+
+    public PnAuditLogEvent generateResult(PnAuditLogType level, String message, List<GeneralMetric> metricsArray, Object... arguments) {
+        PnAuditLogEvent resultEvent = new PnAuditLogEvent(type, mdc, message, metricsArray, arguments);
         resultEvent.originEvent = this;
         resultEvent.level = level;
         return resultEvent;
