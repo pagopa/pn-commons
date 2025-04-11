f369c4964ea3ec1704b912719e4f660c9bebbd45
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2025-04-11 15:23:25 +0200

    [PN-14565] Feat: Added metric object for PNF format

---
+++ src/main/java/it/pagopa/pn/commons/log/dto/metrics/MetricPnf.java
@@ -0,0 +1,14 @@
+package it.pagopa.pn.commons.log.dto.metrics;
+
+public class MetricPnf extends Metric {
+
+    MetricPnf (Metric metric) {
+        super(metric.getName(), metric.getValue());
+    }
+
+    @Override
+    public String toJson() {
+        return String.format("\"Name\":\"%s\",\"Value\":\"%s\"", getName(), getValue());
+    }
+
+}
