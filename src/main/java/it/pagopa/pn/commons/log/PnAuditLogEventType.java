afd79b454ba01ebb775172a590eec2ac049612f3
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2026-06-10 15:59:17 +0200

    Aggiunta nuove tipologie di audit log per API download notifiche esposte ai mittenti

--- src/main/java/it/pagopa/pn/commons/log/PnAuditLogEventType.java
+++ src/main/java/it/pagopa/pn/commons/log/PnAuditLogEventType.java
@@ -114,7 +114,9 @@ public enum PnAuditLogEventType {
     AUD_COM_TIMELINE(PnAuditLogMarker.AUDIT10Y),
     AUD_COM_CHECK(PnAuditLogMarker.AUDIT10Y),
     AUD_COM_VALID(PnAuditLogMarker.AUDIT10Y),
-    AUD_DELAYER_RESCHEDULED_NEXT_WEEK(PnAuditLogMarker.AUDIT10Y);
+    AUD_DELAYER_RESCHEDULED_NEXT_WEEK(PnAuditLogMarker.AUDIT10Y),
+    AUD_COM_DOCOPEN_RCP(PnAuditLogMarker.AUDIT10Y),
+    AUD_COM_ATCHOPEN_RCP(PnAuditLogMarker.AUDIT5Y);
 
     
     final org.slf4j.Marker marker;
