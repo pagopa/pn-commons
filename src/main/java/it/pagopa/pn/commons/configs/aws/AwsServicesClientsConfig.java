a0676fa7846e0417759ad47a4597bf9adf2f379b
Vincenzo Racca <vincenzo.racca@pagopa.it>
2026-06-23 16:09:09 +0200

    feat(PN-20395): update AwsServicesClientsConfig for Project Leyden

--- src/main/java/it/pagopa/pn/commons/configs/aws/AwsServicesClientsConfig.java
+++ src/main/java/it/pagopa/pn/commons/configs/aws/AwsServicesClientsConfig.java
@@ -78,7 +78,7 @@ public class AwsServicesClientsConfig {
     }
 
     private <C> C configureBuilder(AwsClientBuilder<?, C> builder) {
-        if( props != null ) {
+        if( props != null && isLocal() ) {
 
             String profileName = props.getProfileName();
             if( StringUtils.isNotBlank( profileName ) ) {
@@ -114,6 +114,10 @@ public class AwsServicesClientsConfig {
         return DynamoDbEnhancedClient.builder()
                 .dynamoDbClient( baseClient )
                 .build();
+    }
+
+    private boolean isLocal() {
+        return System.getenv("AWS_REGIONCODE") == null;
     }
 
 }
