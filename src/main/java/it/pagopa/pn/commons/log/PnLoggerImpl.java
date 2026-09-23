96e6df37ccd57194f4c5a5b85b8a4c1b9cd31896
Vincenzo Racca <vincenzo.racca@pagopa.it>
2026-09-23 12:35:52 +0200

    increment minor version

--- src/main/java/it/pagopa/pn/commons/log/PnLoggerImpl.java
+++ src/main/java/it/pagopa/pn/commons/log/PnLoggerImpl.java
@@ -10,7 +10,6 @@ import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.TransactWriteItem;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
